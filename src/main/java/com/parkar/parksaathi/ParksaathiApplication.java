8a91b3997db9e2a8d7e8ef8c1011cc494699bacd
Abhijeet Upadhyay <abhijeetupadhyay73@gmail.com>
2026-04-26 23:02:28 +0530

    initial commit

---
+++ src/main/java/com/parkar/parksaathi/ParksaathiApplication.java
@@ -0,0 +1,13 @@
+package com.parkar.parksaathi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParksaathiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParksaathiApplication.class, args);
+	}
+
+}
